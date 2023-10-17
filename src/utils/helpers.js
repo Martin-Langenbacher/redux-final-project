10ef2ce28e9cf6cf6c06abe97cd5ae95f1557581
Martin Langenbacher <martin.langenbacher@telekom.de>
2023-10-17 11:12:29 +0200

    small changes - before really starting

---
+++ src/utils/helpers.js
@@ -0,0 +1,9 @@
+// TODO: Maybe not needed !!!
+
+export function formatDate(timestamp) {
+  return null;
+}
+
+export function formatQuestionML({ optionOneText, optionTwoText, author }) {
+  return null;
+}
