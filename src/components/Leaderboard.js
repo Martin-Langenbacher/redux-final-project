0c27769d78cfe126534208361c8b55473270f9aa
Martin Langenbacher <martin.langenbacher@telekom.de>
2023-10-23 17:54:26 +0200

    PollPage, plus starting the Leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -1,5 +1,75 @@
-const Leaderboard = () => {
-  return <div>Leaderboard</div>;
+import { connect } from "react-redux";
+
+const Leaderboard = (props) => {
+  console.log("Leaderboard (authUser): ", props.authUser);
+  console.log("Leaderboard (questions): ", props.questions);
+  console.log("Leaderboard (users): ", props.users);
+  console.log("Leaderboard (userIDs): ", props.userId);
+  console.log("Leaderboard (userIDs) >>>: ", props.blaIdML);
+
+  const entries = Object.entries(props.users);
+  entries.forEach(([key, value]) => {
+    console.log(`${key}: ${value.id}`);
+    console.log(`${key}: ${value.name}`);
+    console.log(`${key}: ${value.avatarURL}`);
+    console.log(`${key}: ${value.questions.length}`);
+  });
+
+  // props.users[0].forEach(element => {
+  //   console.log(element)
+
+  // });
+
+  return (
+    <div className="table-wrapper">
+      <table className="table-border">
+        <thead>
+          <tr>
+            <th>Users</th>
+            <th>Answered</th>
+            <th>Created</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* const authorOfQuestionURL = props.users[props.questions[id].author].avatarURL; */}
+
+          {props.userId.map((user) => {
+            return (
+              <tr>
+                <td>
+                  {" "}
+                  <div className="picture-special">
+                    <img
+                      src={props.user?.avatarURL}
+                      alt={`Avatar of ${props.authUser}`}
+                      className="user-iconSmall"
+                    />
+                  </div>
+                  <div>Sarah Edo</div>
+                  {user}
+                </td>
+                <td>4</td>
+                <td>2</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
-export default Leaderboard;
+const mapStateToProps = ({ authedUser, users, questions }) => {
+  return {
+    authUser: authedUser,
+    userId: Object.keys(users),
+    blaIdML: Object.values(users),
+    // avatar: users[authedUser],
+    questions: questions,
+    users,
+  };
+};
+
+export default connect(mapStateToProps)(Leaderboard);
+
+// export default connect(mapStateToProps)(PollPage);
--- src/components/PollPage.js
+++ src/components/PollPage.js
@@ -5,13 +5,14 @@ const PollPage = (props) => {
   const { id } = useParams();
   const questionText1 = props.questions[id].optionOne.text;
   const questionText2 = props.questions[id].optionTwo.text;
+  const authorOfQuestion = props.questions[id].author;
 
-  const authorOfQuestionURL = props.users[props.questions[id].author].avatarURL;
+  const authorOfQuestionURL = props.users[authorOfQuestion].avatarURL;
 
   return (
     <div>
       <div className="center">
-        <h2>{`Poll by ${props.questions[id].author}`}</h2>
+        <h2>{`Poll by ${authorOfQuestion}`}</h2>
 
         <div className="big-picture-container">
           <img
@@ -24,14 +25,20 @@ const PollPage = (props) => {
 
       <div>
         <div className="center">
-          <h2>Would You Rather</h2>
-          <div>
-            <div>{questionText1}</div>
-            <button>Click</button>
-          </div>
-          <div>
-            <div>{questionText2}</div>
-            <button>Click</button>
+          <h2 className="wouldYouRather">Would You Rather</h2>
+          <div className="container-poll-page">
+            <div className="poll-text-left">
+              <div className="poll-text-text">{questionText1}</div>
+              <button className="btn-poll" type="submit">
+                Click
+              </button>
+            </div>
+            <div className="poll-text-right">
+              <div className="poll-text-text">{questionText2}</div>
+              <button className="btn-poll" type="submit">
+                Click
+              </button>
+            </div>
           </div>
         </div>
       </div>
