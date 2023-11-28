f25092e276815a35ee6c04376f1e1609a7437c90
Martin Langenbacher <martin.langenbacher@telekom.de>
2023-11-28 13:58:14 +0100

    toBe does not work in POLLPage.test.js - but WHY, why

--- src/components/PollPage.test.js
+++ src/components/PollPage.test.js
@@ -1,31 +1,20 @@
 import * as React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import { Route, Routes } from "react-router-dom";
-//import { MemoryRouter } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { createStore } from "redux";
-
-import { setAuthedUser } from "../actions/authedUser";
 
 import reducer from "../reducers";
 import middleware from "../middleware";
 import PollPage from "./PollPage";
 import { Provider } from "react-redux";
 
-// Mock the useSelector and useDispatch hooks
-/*
-jest.mock("react-redux", () => ({
-  ...jest.requireActual("react-redux"),
-  useSelector: jest.fn(),
-  useDispatch: jest.fn(),
-}));
-*/
 describe("PollPage - DOM test", () => {
   it('6 --> will display "Your answer", after pressing the optionOne button', () => {
-
-    /*
     console.log("TODO");
 
+
+    /* TODO: ===========>>> To test: take this line out! 
     const store = createStore(reducer, middleware);
 
     const userId = "sarahedo";
@@ -34,10 +23,8 @@ describe("PollPage - DOM test", () => {
     var view = render(
       <Provider store={store}>
         <Router initialEntries={[`/question/${questionId}`]}>
-          
           <Routes>
             <Route path="/question/:id" />
-            
           </Routes>
           <PollPage />
         </Router>
@@ -60,56 +47,13 @@ describe("PollPage - DOM test", () => {
     const question = state.questions[questionId];
 
     // Todo: Hier weitermachen !!!!!
-    console.log('Todo: Continue here !!!!! state:', state)
-    console.log('Todo: Continue here !!!!! user:', user)
+    console.log("Todo: Continue here !!!!! state:", state);
+    console.log("Todo: Continue here !!!!! user:", user);
 
     expect(user.answers[questionId]).toBe("optionOne");
 
-    */
-
+    TODO: ===========>>> To test: take this line out! */
   });
 });
 
-// TODO: Not working yet !!!!
 
-/*
-describe("PollPage - DOM test", () => {
-
-
-  it('6 --> will display "Your answer", after pressing the optionOne button', () => {
-    const store = createStore(reducer, middleware);
-
-    const username = "zoshikanlu";
-
-    store.dispatch(setAuthedUser(username));
-
-    jest.mock("react-router-dom", () => ({
-      ...jest.requireActual("react-router-dom"),
-      useParams: jest.fn().mockReturnValue({ id: "8xf0y6ziyjabvozdd253nd" }),
-    }));
-
-    const { getByTestId } = render(
-      <Provider store={store}>
-        <Router>
-          <Routes>
-            <Route path="/question/:id" element={<PollPage />} />
-          </Routes>
-        </Router>
-      </Provider>
-    );
-
-    // eslint-disable-next-line testing-library/prefer-screen-queries
-    const optionOneButton = getByTestId("optionOne-button");
-    fireEvent.click(optionOneButton);
-
-    // eslint-disable-next-line testing-library/prefer-screen-queries
-    expect(getByTestId("success-header")).toBeInTheDocument();
-
-/*
-
-
-  });
-  
-});
-
-*/
